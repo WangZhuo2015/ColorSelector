586e558591839da3c953044b892852f4cf6a3619
WangZhuo <wangzhuo@WangZhuodeMacBook-Pro.local>
2015-07-10 23:11:08 -0700

    Initial Commit

---
+++ ColorSelector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorSelector
+//
+//  Created by WangZhuo on 15/7/10.
+//  Copyright © 2015年 WangZhuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
